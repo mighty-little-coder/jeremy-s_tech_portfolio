943fba3a0a1930952425d3a2eb933975867c09fb
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-03-09 04:20:42 -0500

    Added About Me and contact form

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,63 +1,24 @@
-// This is a static page mocking an "About Us" section for our fake user data
 export default function AboutPage() {
   return (
-    <div className="container pt-4">
-      <ul className="aboutMe_title">
-        <li>Hello!</li>
-        <li>I'm Jeremy.</li>
+    <div className="aboutMe_container">
+      <div className="aboutMe_header">
+        <img src="src/assets/images/IMG_20190710_090801_337.jpg" alt="Jeremy Doran" className="aboutMe_img" />
+        <ul className="aboutMe_title">
+          <li>Hello!</li>
+          <li>I'm Jeremy.</li>
         </ul>
-      <p>
-        I am a Connecticut native and a recent graduate of the University of Connecticut's Full Stack Web Development Bootcamp. I am a passionate developer with a strong work-ethic and a desire to create user-friendly applications. I am a quick learner and a team player who is eager to contribute to a team and learn from others. I am excited to bring my skills to a new team and continue to grow as a developer. I have spent most of my professional career in blue collar industries as a mechanic and a hydraulic assembler on aerospace equipment. In the meantime, I have been learning many additional, mostly creative skills outside of my time in the workforce. I am a proficient dancer, videographer and mechanic and i look forward to bringing my unique skills to a new team that i can grow with!
-      </p>
-      <section className="features-icons bg-light text-center m-4">
-        <div className="container">
-          <div className="row p-2">
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-window m-auto text-primary" />
-                </div>
-                <h3>Fully Responsive</h3>
-                <p className="lead mb-0">
-                  This theme will look great on any device, no matter the size!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Bootstrap 5 Ready</h3>
-                <p className="lead mb-0">
-                  Featuring the latest build of the new Bootstrap 5 framework!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Easy to Use</h3>
-                <p className="lead mb-0">
-                  Ready to use with your own content, or customize the source
-                  files!
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      </div>
+      <section className="aboutMe_blurb">
+        <p>
+          I am a Connecticut native and a recent graduate of the University of Connecticut's Full Stack Web Development Bootcamp. I am a passionate developer with a strong work-ethic and a desire to create user-friendly applications. I am a quick learner and a team player who is eager to contribute to a team and learn from others.
+        </p>
+        <p>
+          I am excited to bring my skills to a new team and continue to grow as a developer. I have spent most of my professional career in blue collar industries as a mechanic and a hydraulic assembler on aerospace equipment. In the meantime, I have been learning many additional, mostly creative skills outside of my time in the workforce. I am a proficient dancer, videographer and mechanic and I look forward to bringing my unique skills to a new team that I can grow with!
+        </p>
+        <p>
+          In summary, I believe I would be a good fit for most any team. I enjoy the connection of working with others and am always eager to learn more! I enjoy getting to dig into an individual topic or project so that i can continue to look at it from new angles. I always aim to improve the process by which I accomplish tasks and am excited at the prospect of doing that within a team of people that are knowledgeable and passionate about what they do.
+        </p>
       </section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,17 +3,8 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="contact-content">
-      <h1>Contact Me</h1>
+      <h1>Connect with Me!</h1>
       <p>Feel free to fill out the form below to connect with me, or connect with me on LinkedIn or GitHub.</p>
-      {/* <form>
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name" required></input>
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required></input>
-          <label for="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
-          <input type="submit" value="Submit"></input>
-        </form> */}
       <form>
         <div className="form_group">
           <label htmlFor="name">Name:</label>
