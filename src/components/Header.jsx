e58c476fca56d4e32d96d4063dffcab8bfe8b7b1
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-03-10 06:38:56 -0400

    Made mobile friendly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
+  const [activeButton, setActiveButton] = useState('');
+
+  useEffect(() => {
+    setActiveButton(location.pathname);
+  }, [location.pathname]);
+
   return (
     <header className='header_content'>
       <h1>Jeremy Doran</h1>
       <h2>Professional Portfolio</h2>
       <nav className='navbar_content'>
         <ul className='buttons'>
-          <li><a href="/">About</a></li>
-          <li><a href="/projects">Projects</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="/resume">Resume</a></li>
+          <li className={activeButton === '/' ? 'active' : ''}>
+            <Link to="/">About</Link>
+          </li>
+          <li className={activeButton === '/projects' ? 'active' : ''}>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className={activeButton === '/contact' ? 'active' : ''}>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className={activeButton === '/resume' ? 'active' : ''}>
+            <Link to="/resume">Resume</Link>
+          </li>
         </ul>
       </nav>
     </header>
